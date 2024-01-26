3d14e588bb97be1b53cda811f0eaacabe2f55195
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-26 14:29:42 -0500

    Clean JS file and add TODO items

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('scrolled');
   }
 });
+
+// TODO
+// - Make navbar clicks work (scroll to correct section)
+// - Make a hover over item navbar for which section currently is in view
+// - Maybe some animation for the title "Hi, I'm Kevin", and possibly other sections
+// - Add image scroll for travel images
+// - Possibly maybe more things to do ... 
