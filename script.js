91f50179db558780b4fcf65657377afbab3becc6
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-07-25 18:42:13 +0800

    Fetch weather data for description of Montreal
    Uses Weatherapi's API to fetch the current weather of Montreal and
    displays it in the initial description of the webpage.

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ window.addEventListener('scroll', function () {
   }
 });
 
+// for weather in description
+document.addEventListener('DOMContentLoaded', function() {
+  const apiKey = 'd03e81b7e6e7449d91a103336242507'; 
+  const city = 'Montreal';
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+          const weatherElement = document.querySelector('.weather');
+          const temperature = data.current.temp_c;
+          weatherElement.textContent = `${temperature}°C`;
+      })
+      .catch(error => { // if error, log to console
+          console.error('Error fetching weather data:', error);
+      });
+});
+
+
 // TODO
 // - Make navbar clicks work (scroll to correct section)
 // - Make a hover over item navbar for which section currently is in view
