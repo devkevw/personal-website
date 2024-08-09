d453331927d24cf78fc13a5bff3e032488b12c37
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-08-08 21:31:05 -0400

    Add styles and functions to navbar
    This includes bolding and highlighting the current section of the page
    in the navbar, allowing users to click on sections of the navbar to
    jump to the section of the page, and various formatting.

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 // navbar scrolling 
 const navbar = document.querySelector('.bar');
+const navbarLinks = document.querySelectorAll('.bar a');
 
 window.addEventListener('scroll', function () {
+  // change navbar color once scrolled down
   if (window.scrollY > 50) {
     navbar.classList.add('scrolled');
   } else {
     navbar.classList.remove('scrolled');
   }
+
+  // for highlighting each section in navbar
+  let current = '';
+
+  // loop through each section with the class `page-section`
+  document.querySelectorAll('.page-section').forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (window.scrollY >= sectionTop - 50) { 
+      current = section.getAttribute('id');
+    }
+  });
+
+  // remove active class from all links
+  navbarLinks.forEach(link => {
+    link.parentElement.classList.remove('active');
+  });
+
+  // check if user scrolled to the bottom
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) { // add active class to mailto link
+    document.querySelector('.bar a[href^="mailto:"]').parentElement.classList.add('active');
+  } else { // add active class to current link
+    document.querySelector(`.bar a[href*="${current}"]`).parentElement.classList.add('active');
+  }
 });
+
 
 // for weather in description
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,16 +42,17 @@ document.addEventListener('DOMContentLoaded', function() {
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
   fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-          const weatherElement = document.querySelector('.weather');
-          const temperature = data.current.temp_c;
-          weatherElement.textContent = `${temperature}°C`;
-      })
-      .catch(error => { // if error, log to console
-          console.error('Error fetching weather data:', error);
-      });
+    .then(response => response.json())
+    .then(data => {
+      const weatherElement = document.querySelector('.weather');
+      const temperature = data.current.temp_c;
+      weatherElement.textContent = `${temperature}°C`;
+    })
+    .catch(error => { // if error, log to console
+      console.error('Error fetching weather data:', error);
+    });
 });
+
 
 
 // TODO
